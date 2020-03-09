a35bb93f280dbf57b16c2ee880aa09292c66b819
luiskik10 <luislocoxcristo@gmail.com>
2020-03-08 23:16:57 -0600

    primer comit
    inicio del proyecto

---
+++ resources/layouts/app.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <!-- Bootstrap CSS -->
+    <title>megamovie = @yield('title')</title>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/portada.css') }}" rel="stylesheet">
+  </head>
+  <body style="background-image: url(/images/estrellas.gif);">
+
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark text-white">
+        <a class="navbar-brand" href="#">www.movie-mega-mp4.com</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Desabilidato</a>
+            </li>
+             <li class="nav-item">
+               <a class="nav-link " href="#">Ingresar</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link " href="#">Registrar</a>
+             </li>
+          </ul>  
+        </div>
+      </nav>
+    </header>
+
+ <div class="jumbotron text-center" style="max-height:75vh; background-image:url(/images/estrellas.gif);">
+        <!-- portada -->
+	     <img src="/images/portada limpia.png"  style=" position: relative; width: 90vw; max-height:70vh; min-height: 30vh;" >   
+				<div id="wrapper">
+				  <p id="stars"><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span></p>
+				  <p id="title" contenteditable="true" spellcheck="false"><span>Movie</span><span>-mega</span><span>-mp4.</span><span>com</span></p>
+				  <p id="slogan"><span>Peliculas, Programas y más...</span></p>
+				</div>
+</div>
+    
+    <main role="main">
+		<!-- contenedor -->      
+		<div  style="background-image: url(/images/fondo.jpg); "  class="container marketing"> 
+		      </br>
+
+		        @yield('content')
+		</div>
+		<!-- /fin de contenedor -->
+
+
+      <!-- FOOTER -->
+      <footer class="container">
+        <p class="float-right"><a href="#">Inicio</a></p>
+        <p class="text-white">&copy; 2019 Moviemega, Inc. &middot; <a href="#">TODO LOS DERECHOS RESERVADOS</a> &middot; <a href="#">WWW.movie-mega-mp4.COM</a></p>
+      </footer>
+      <!-- fin FOOTER -->
+    
+    </main>
+
+  
+       <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
+       <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+       <script src="{{ asset('js/popper.min.js') }}"></script>
+
+  </body>
+
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('title', 'Ver pelicula')
+@section('content')
+@section('css')
+   <link href="{{ asset('css/dropzone.css') }}" rel="stylesheet">
+@endsection
+  
+
+ <div class="row">
+   <div class="col-lg-12 text-white text-center">
+        
+          
+        
+
+   </div>
+</div>
+
+@endsection
+
