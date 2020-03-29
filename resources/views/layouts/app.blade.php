a8a1eef812679def92b2e707d74a65638c140668
luiskik10 <luislocoxcristo@gmail.com>
2020-03-29 15:06:08 -0600

    cambio de home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,77 +1,73 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <!-- Bootstrap CSS -->
-    <title>@yield('title')</title>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/portada.css') }}" rel="stylesheet">
-  </head>
-  <body style="background-image: url(/images/estrellas.gif);">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>@yield('title')</title>
 
-    <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark text-white">
-        <a class="navbar-brand" href="#">Colegio campo y vida</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Desabilidato</a>
-            </li>
-             <li class="nav-item">
-               <a class="nav-link " href="#">Ingresar</a>
-             </li>
-             <li class="nav-item">
-               <a class="nav-link " href="#">Registrar</a>
-             </li>
-          </ul>  
+  <!-- CSS  -->
+  <link href="{{ asset('css/letras.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/materialize.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="{{ asset('css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">Logo</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+   @yield('content')
+
+  <footer class="page-footer teal">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
         </div>
-      </nav>
-    </header>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
 
- <div class="jumbotron text-center" style="max-height:75vh; background-image:url(/images/estrellas.gif);">
-        <!-- portada -->
-	     <img src="/images/portada limpia.png"  style=" position: relative; width: 90vw; max-height:70vh; min-height: 30vh;" >   
-				<div id="wrapper">
-				  <p id="title" contenteditable="true" spellcheck="false"><span>Colegio </span><span>Ecologico </span><span>Particular </span><span>Mixto </span><span>Campo y </span><span>Vida </span></p>
-				  <p id="slogan"><span>Haz rodar sobre Jehová tus obras y tus planes seran estrablecidos</span></p>
-				</div>
- </div>
-    
-    <main role="main">
-		<!-- contenedor -->      
-		<div  style="background-image: url(/images/fondo.jpg); "  class="container marketing"> 
-		      </br>
 
-		        @yield('content')
-		</div>
-		<!-- /fin de contenedor -->
-
-
-      <!-- FOOTER -->
-      <footer class="container">
-        <p class="float-right"><a href="#">Inicio</a></p>
-        <p class="text-white">&copy; 2019 Moviemega, Inc. &middot; <a href="#">TODO LOS DERECHOS RESERVADOS</a> &middot; <a href="#">WWW.movie-mega-mp4.COM</a></p>
-      </footer>
-      <!-- fin FOOTER -->
-    
-    </main>
-
-  
-       <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
-       <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-       <script src="{{ asset('js/popper.min.js') }}"></script>
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="{{ asset('js/materialize.js') }}"></script>
+  <script src="{{ asset('js/init.js') }}"></script>
 
   </body>
-
-
 </html>
